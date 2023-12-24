2fe64c4f15a481a55bd72e1d7205768bd248c096
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-24 21:19:23 +0700

    Fix styling in Home component

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -10,7 +10,7 @@ const Home = () => {
   return (
     <Layout>
       <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
-        <div className="px-4 py-5 bg-white  rounded-[17px] shadow-md">
+        <div className="px-4 py-5 bg-white rounded-[17px] shadow-md">
           <div className="flex mb-2 items-center justify-between">
             <div className="text-2xl font-semibold">Recent Activity</div>
             <button className=" text-2xl flex flex-wrap gap-4 justify-center">
