59eb21c2b0de9baaea7db70cfa20f3783dffc8f1
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-25 21:57:15 +0700

    feat: Add AddActivity component to Activity page

--- src/pages/Activity/index.jsx
+++ src/pages/Activity/index.jsx
@@ -3,6 +3,7 @@ import { instance } from "../../utils/instance";
 import Layout from "../Layout";
 import ActivityCard from "../../components/molecules/ActivityCard";
 import { useAuthHeader } from "react-auth-kit";
+import AddActivity from "../../components/molecules/Modal/AddActivity";
 
 const Activity = () => {
   const authHeader = useAuthHeader();
@@ -30,7 +31,12 @@ const Activity = () => {
     <Layout>
       <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
         <div className="px-4 py-4 bg-white rounded-[17px] shadow-md">
-          <div className="text-2xl mb-2 font-semibold">Activity</div>
+        <div className="flex mb-5 items-center justify-between">
+          <div className="text-2xl font-semibold">Activity</div>
+          <button className=" text-2xl flex flex-wrap gap-4 justify-center">
+            <AddActivity />
+          </button>
+        </div>
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 gap-4 justify-items-center">
             {data.map((item) => {
               return (
