cde0e75286ff99f6f5ee82ed3a39c67b7e96b462
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-25 22:18:32 +0700

    fix: Update goal input to textarea in ContentActivity.jsx

--- src/pages/ActivityDetail/child/ContentActivity.jsx
+++ src/pages/ActivityDetail/child/ContentActivity.jsx
@@ -169,11 +169,12 @@ const ContentActivity = ({ data }) => {
               >
                 Goal
               </label>
-              <input
+              <textarea
                 type="text"
                 id="goal"
                 onChange={formik.handleChange}
                 name="goal"
+                rows="4"
                 value={formik.values.goal}
                 className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 placeholder="Goal"
