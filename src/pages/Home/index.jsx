508db2ea8604247a4418d53d2821383b012d011c
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-27 06:36:44 +0700

    feat: Add DietPlanList component to Home page

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,6 +2,7 @@ import Layout from "../Layout";
 import ActivityList from "../../components/molecules/ActivityList";
 import TimeManagementList from "../../components/molecules/TimeManagementList";
 import FinancialRecordList from "../../components/molecules/FinancialRecordList";
+import DietPlanList from "../../components/molecules/DietPlanList";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <ActivityList />
       <TimeManagementList />
       <FinancialRecordList />
+      <DietPlanList />
     </Layout>
   );
 };
