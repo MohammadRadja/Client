90f3fdf48ed44877713024c4da1f0705ef457172
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-27 15:53:07 +0700

    refactor: modal buttons in AddActivity, AddDietPlan, AddFinancialRecord, and AddTimeManagement components

--- src/components/molecules/Modal/AddFinancialRecord.jsx
+++ src/components/molecules/Modal/AddFinancialRecord.jsx
@@ -54,13 +54,13 @@ const AddFinancialRecord = () => {
   return (
     <div>
       {/* Modal toggle */}
-      <button
+      <span
         onClick={toggleModal}
         className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
         Add Financial Record
-      </button>
+      </span>
 
       {/* Main modal */}
       {showModal && (
