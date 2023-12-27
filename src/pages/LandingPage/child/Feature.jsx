f1d445085ce134f846cbefe21a8305a7b086b5d0
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-27 16:29:59 +0700

    styling: Jumbotron component

--- src/pages/LandingPage/child/Feature.jsx
+++ src/pages/LandingPage/child/Feature.jsx
@@ -1,4 +1,4 @@
-import { BiListCheck, BiTime, BiMoney} from "react-icons/bi";
+import { BiListCheck, BiTime, BiMoney, BiNews } from "react-icons/bi";
 import { GiWeightLiftingUp } from "react-icons/gi";
 
 function YourComponent() {
@@ -6,90 +6,42 @@ function YourComponent() {
     <section className="bg-gray-50 dark:bg-gray-800">
       <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
         <div className="mb-8 max-w-screen-md lg:mb-16">
-        <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
-          Eksplorasi Fitur Unggul <span className="text-blue-600">LifeSyncHub</span>
-        </h2>
+          <h2 className="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">
+            Eksplorasi Fitur Unggul{" "}
+            <span className="text-blue-600">LifeSyncHub</span>
+          </h2>
           <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-          LifeSyncHub adalah aplikasi yang membantu pengguna dalam mengelola kehidupan sehari-hari dengan 4 fitur utama: 
-          Aktivitas, Manajemen Waktu, Diet Plan, dan Kesehatan Fisik. 
+            LifeSyncHub adalah aplikasi yang membantu pengguna dalam mengelola
+            kehidupan sehari-hari dengan 4 fitur utama: Aktivitas, Manajemen
+            Waktu, Diet Plan, dan Kesehatan Fisik.
           </p>
         </div>
         <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
           {/* Card 1 */}
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* <path
-                  fillRule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clipRule="evenodd"
-                ></path> */}
-                <BiListCheck />
-              </svg>
-
-              {/* SVG icon content */}
-            </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Activity
-            </h3>
+            <BiListCheck className="text-5xl" />
+            <h3 className="mb-2 text-xl font-bold dark:text-white">Activity</h3>
             <p className="text-gray-500 dark:text-gray-400">
-              Fitur Activity memungkinkan pengguna membuat dan menyimpan catatan penting.
+              Fitur Activity memungkinkan pengguna membuat dan menyimpan catatan
+              penting.
             </p>
           </div>
           {/* End of Card 1 */}
 
           {/* Card 2 */}
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                className="w-10 h-10 lg:w-10 lg:h-10"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* <path
-                  fillRule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clipRule="evenodd"
-                ></path> */}
-                <BiTime />
-              </svg>
-
-              {/* SVG icon content */}
-            </div>
             <h3 className="mb-2 text-xl font-bold dark:text-white">
+              <BiTime className="text-5xl" />
               Time Management
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-              Fitur Manajemen Waktu memungkinkan pengguna untuk membuat jadwal dan mengatur prioritas.
+              Fitur Manajemen Waktu memungkinkan pengguna untuk membuat jadwal
+              dan mengatur prioritas.
             </p>
           </div>
-          {/* End of Card 2 */}
 
-          {/* Card 3 */}
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* <path
-                  fill-rule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clip-rule="evenodd"
-                ></path> */}
-                <BiMoney />
-              </svg>
-
-              {/* SVG icon content */}
-            </div>
+            <BiMoney className="text-5xl" />
             <h3 className="mb-2 text-xl font-bold dark:text-white">
               Financial Record
             </h3>
@@ -97,93 +49,25 @@ function YourComponent() {
               Fitur Kesehatan Fisik memberikan berita kesehatan terbaru.
             </p>
           </div>
-          {/* End of Card 3 */}
 
-          {/* Card 4 */}
           <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                {/* <path
-                  fill-rule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clip-rule="evenodd"
-                ></path> */}
-                <GiWeightLiftingUp />
-              </svg>
-
-              {/* SVG icon content */}
-            </div>
+            <GiWeightLiftingUp className="text-5xl" />
             <h3 className="mb-2 text-xl font-bold dark:text-white">
               Diet Plan
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-                Fitur Diet Plan mencatat detail konsumsi makanan harian.
+              Fitur Diet Plan mencatat detail konsumsi makanan harian.
             </p>
           </div>
-          {/* End of Card 4 */}
-
-          {/* Card 5 */}
-          {/* <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg> */}
-
-              {/* SVG icon content */}
-            {/* </div>
+          <div>
+            <BiNews className="text-5xl" />
             <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Marketing
+              Health Article
             </h3>
             <p className="text-gray-500 dark:text-gray-400">
-              Plan it, create it, launch it. Collaborate seamlessly with all the
-              organization and hit your marketing goals every month with our
-              marketing plan.
+              Fitur Health Article memberikan berita kesehatan terbaru.
             </p>
-          </div> */}
-          {/* End of Card 5 */}
-
-          {/* Card 6 */}
-          {/* <div>
-            <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-              <svg
-                class="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M3 3a1 1 0 000 2v8a2 2 0 002 2h2.586l-1.293 1.293a1 1 0 101.414 1.414L10 15.414l2.293 2.293a1 1 0 001.414-1.414L12.414 15H15a2 2 0 002-2V5a1 1 0 100-2H3zm11.707 4.707a1 1 0 00-1.414-1.414L10 9.586 8.707 8.293a1 1 0 00-1.414 0l-2 2a1 1 0 101.414 1.414L8 10.414l1.293 1.293a1 1 0 001.414 0l4-4z"
-                  clip-rule="evenodd"
-                ></path>
-              </svg> */}
-
-              {/* SVG icon content */}
-            {/* </div>
-            <h3 className="mb-2 text-xl font-bold dark:text-white">
-              Marketing
-            </h3>
-            <p className="text-gray-500 dark:text-gray-400">
-              Plan it, create it, launch it. Collaborate seamlessly with all the
-              organization and hit your marketing goals every month with our
-              marketing plan.
-            </p>
-          </div> */}
-          {/* End of Card 6 */}
+          </div>
         </div>
       </div>
     </section>
--- src/pages/LandingPage/child/Jumbotron.jsx
+++ src/pages/LandingPage/child/Jumbotron.jsx
@@ -1,21 +1,24 @@
-
 const Jumbotron = () => {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="grid py-8 px-4 mx-auto max-w-screen-xl lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="place-self-center mr-auto lg:col-span-7">
-        <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-gray-900 dark:text-white">
-          Optimalkan Hidup Anda dengan <span className="text-blue-600">LifeSyncHub</span>
-        </h1>
+          <h1 className="mb-4 max-w-2xl text-4xl font-extrabold leading-none md:text-5xl xl:text-6xl text-gray-900 dark:text-white">
+            Optimalkan Hidup Anda dengan{" "}
+            <span className="text-blue-600">LifeSyncHub</span>
+          </h1>
           <p className="mb-6 max-w-2xl font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
-          LifeSyncHub adalah platform yang dirancang untuk membantu pengguna menyelaraskan dan mengelola berbagai aspek kehidupan mereka. 
-          Dengan fokus pada kesejahteraan umum, platform ini memungkinkan pengguna untuk mengintegrasikan keuangan, kesehatan fisik, dan manajemen waktu. 
-          Sebagai pusat sumber daya, 
-          LifeSyncHub memberikan alat dan informasi yang memungkinkan pengguna mencapai keseimbangan dan koordinasi dalam kehidupan mereka secara menyeluruh.
+            LifeSyncHub adalah platform yang dirancang untuk membantu pengguna
+            menyelaraskan dan mengelola berbagai aspek kehidupan mereka. Dengan
+            fokus pada kesejahteraan umum, platform ini memungkinkan pengguna
+            untuk mengintegrasikan keuangan, kesehatan fisik, dan manajemen
+            waktu. Sebagai pusat sumber daya, LifeSyncHub memberikan alat dan
+            informasi yang memungkinkan pengguna mencapai keseimbangan dan
+            koordinasi dalam kehidupan mereka secara menyeluruh.
           </p>
           <a
             href="/login"
-            className="hidden lg:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-blue-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 mr-2"
+            className="hidden lg:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-primary rounded-lg border border-primary hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800 mr-2"
           >
             Login
             <svg
@@ -33,16 +36,13 @@ const Jumbotron = () => {
           </a>
           <a
             href="/register"
-            className="hidden lg:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-blue-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+            className="hidden lg:inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-primary rounded-lg border border-primary hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
           >
             Register
           </a>
         </div>
         <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-          <img
-            src="/images/jumbotron.png"
-            alt="mockup"
-          />
+          <img src="/images/jumbotron.png" alt="mockup" />
         </div>
       </div>
     </section>
