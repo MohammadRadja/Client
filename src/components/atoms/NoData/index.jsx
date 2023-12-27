608c01592a5fff42db352a4260da83de28ac1885
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-27 09:06:23 +0700

    styling: Update NoData component styling

--- src/components/atoms/NoData/index.jsx
+++ src/components/atoms/NoData/index.jsx
@@ -1,7 +1,7 @@
 const NoData = () => {
   return (
     <div className="container mx-auto flex flex-col items-center px-4 text-center md:px-10 lg:px-32 xl:max-w-3xl">
-      <h1 className="text-2xl font-semibold sm:text-xl">
+      <h1 className="text-xl font-semibold sm:text-xl">
         No <span className="text-blue-600 font-bold">Data </span>Added Yet.
       </h1>
     </div>
