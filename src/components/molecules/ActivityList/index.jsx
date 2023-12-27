8f6375dea5629c7b7e30bd6eabe10031b7500091
Uti Tiyanum <48754005+UTYNM@users.noreply.github.com>
2023-12-27 09:08:42 +0700

    styling: font size for activity, diet plan, financial record, and time management headings

--- src/components/molecules/ActivityList/index.jsx
+++ src/components/molecules/ActivityList/index.jsx
@@ -31,7 +31,7 @@ const ActivityList = () => {
     <div className="mx-auto mt-4 max-w-[600px] 2xl:max-w-[800px] mb-10">
       <div className="px-4 py-5 bg-white rounded-[17px] shadow-md">
         <div className="flex mb-2  items-center justify-between">
-          <div className="text-3xl font-semibold">Recent Activity</div>
+          <div className="text-2xl font-semibold">Recent Activity</div>
         </div>
         <div className="relative flex gap-4 py-6 overflow-x-auto">
           {data.length > 0 ? (
